af6f37625bb7b51243a7f103bd57d460ef70d29c
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2023-05-07 11:16:06 +0600

    initiated react environment, added bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="container">
+          <h1 className="text-primary py-20">Homepage</h1>
+          <button className="btn btn-primary mt-3">Click here</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
